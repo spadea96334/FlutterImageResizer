cb3b497d8bf2a202e170a62863c82afd1e5adca3
spadea96334 <spadea96334@gmail.com>
2024-03-15 17:47:26 +0800

    remove unnessnary import

--- lib/widget/progress_dialog.dart
+++ lib/widget/progress_dialog.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 import '../resizer/image_resizer.dart';
 
 class ProgressDialog extends StatelessWidget {
